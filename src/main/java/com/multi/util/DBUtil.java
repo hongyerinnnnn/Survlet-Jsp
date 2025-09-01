bdd17a19a5757ab4c6aad99a909dc8fc251dc10e
Hong Ye Rin <yerinnn526@naver.com>
2025-09-01 17:52:15 +0900

    mvc

--- src/main/java/com/multi/util/DBUtil.java
+++ src/main/java/com/multi/util/DBUtil.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 // 1. JDBC 연결하는 드라이브 코드 설정
 // 2. MVC 구조를 이해하는 코드로 진행
 // 3. Java 부분, JSP 부분 구분해서 프로그램 구현
-// 3. el-jstl을 이용하는 목록을 출력
+// 4. el-jstl을 이용하는 목록을 출력
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/spring";
     private static final String USER = "root";
