175f55c1d6ff3cd43fa118b5b8bcec0a811786e5
Hong Ye Rin <yerinnn526@naver.com>
2025-09-03 11:33:33 +0900

    mvc

--- src/main/java/basic/io/FileIOExample.java
+++ src/main/java/basic/io/FileIOExample.java
@@ -1,4 +1,8 @@
 package basic.io;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+
 /*
 Stream은 흐름을 말함
 Byte를 가지고 흘러가면 ByteStream
@@ -6,9 +10,14 @@ Byte를 가지고 흘러가면 ByteStream
 public class FileIOExample { //ByteStream관련
     public static void main(String[] args) {
         try{
+            FileOutputStream fos = new FileOutputStream("c:\\iowrite.txt");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            String content = "Hello World!";
+            bos.write(content.getBytes());
+            bos.flush();
 
         }catch(Exception e){
-
+            e.printStackTrace();
         }
     }
 }
