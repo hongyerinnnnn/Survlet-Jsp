3410e1d2f08fffe78783bb283499b066cd6f633d
Hong Ye Rin <yerinnn526@naver.com>
2025-09-05 16:13:45 +0900

    ajax

---
+++ src/main/java/com/multi/controller/TimeServlet.java
@@ -0,0 +1,21 @@
+package com.multi.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/time")
+public class TimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        resp.getWriter().print("현재 시각: " + now);
+    }
+}
